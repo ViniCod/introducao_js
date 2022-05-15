16afbf3511f8a714374733742996432f84677e99
Vinicius <viniciusprates.6798@aluno.saojudas.br>
2022-05-15 19:35:19 -0300

    Operações básicas

---
+++ console/operacoes1.js
@@ -0,0 +1,8 @@
+/* Declarando duas variáveis e fazendo algumas operações básicas */
+var a = 20;
+var b = 10;
+
+console.log('A soma de A e B é: ' + (a + b));
+console.log('A subtração de A por B é: ' + (a - b));
+console.log('A multiplicação de A por B é: ' + (a * b));
+console.log('A divisão de A por B é: ' + (a / b));
